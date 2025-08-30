260bc8a52abd269cee99b2f53f102000fdd70697
praditsoftgit <praditsoft.git@gmail.com>
2025-08-30 23:01:25 +0700

    Apply env file

--- server.js
+++ server.js
@@ -1,28 +1,70 @@
 const express = require('express')
 const expressWs = require('express-ws')
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const app = express()
-const port = 8088
+const port = process.env.PORT
+const api = process.env.SOCKET_API
 expressWs(app);
 const wsClients = [];
+app.use(express.json());
 
 app.listen(port, () => {        
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port} ${api}`)
 })
 
-app.ws('/karaoke', function(ws, request) {
+function parseCookies(cookieString) {
+  const cookies = {};
+  cookieString.split(';').forEach(cookie => {
+    const parts = cookie.split('=');
+    cookies[parts[0].trim()] = parts[1].trim();
+  });
+  return cookies;
+}
 
+function verifyToken(token) {
+    return jwt.verify(token, process.env.JWT_SECRET_KEY, (error, info)=>{
+        if(!error) {
+            // console.log("request.headers -> ", request.headers)
+            // request.body.user = info.username
+            // request.headers["USER_TOKEN"] = info.username
+            return true
+        } else {
+            console.log(error);
+            return false
+        }
+    })
+}
+
+app.ws(`${api}`, function(ws, request) {
+    console.log("WebSocket connection: ", request.query);
+
+    // const cookies = request.headers.cookie;
+    // if(cookies) {
+    //     const parsedCookies = parseCookies(cookies);
+        const token = request.query.token;
+        const isValidToken = verifyToken(token)
+
+        if(isValidToken) {
             wsClients.push(ws);
-            ws.on('close', () => {
-                wsClients.splice(wsClients.indexOf(ws), 1);
-            });
+        } else {
+            console.log('Invalid token!');
+            
+        }
+        ws.on('close', () => {
+            wsClients.splice(wsClients.indexOf(ws), 1);
+        });
 
-            ws.on('message', function(msg) {
-                wsClients.forEach((client) => {
-                    if (client !== ws) {
-                    client.send(msg);
-                    }
-                });
-            });
+    // }
 
+    ws.on('message', function(msg, request) {
+        // const cookies = request.headers.cookie;
+        console.log("WebSocket Server receive message: ", msg);
+        wsClients.forEach((client) => {
+            if (client !== ws) {
+                client.send(msg);
+            }
+        });
+    });
 });
